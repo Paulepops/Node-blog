66a9829d2707574803d864eaeac53b03f3745345
Paulepops <sorroko-dev@outlook.com>
2022-04-24 12:07:18 +1000

    Converted old posts list to post tiles

--- controllers/listPosts.js
+++ controllers/listPosts.js
@@ -2,8 +2,6 @@ const BlogPost = require('../models/BlogPost')
 
 module.exports = async (req,res) => {
     const blogposts = await BlogPost.find({}).populate('userid')
-    // console.log(blogposts)
-    // console.log(req.session)
     res.render('olderPosts',{
         blogposts
     });
